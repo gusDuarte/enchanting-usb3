1d4e75fe2f32112a83f586c2f1a28edb209e392d
Gustavo Duarte <gus.duarte@gmail.com>
2014-12-03 11:45:46 -0200

    Add setting environment script for mac

---
+++ leJOS_NXJ_0.9.1beta-3/mac-set_env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export NXJ_HOME=/Users/gustavo/ceibal/devel/lejos/leJOS_NXJ_0.9.1beta-3
+export LEJOS_NXT_JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+export PATH=$NXJ_HOME/bin:$PATH
--- leJOS_NXJ_0.9.1beta-3/projects/HelloWorld.java
+++ leJOS_NXJ_0.9.1beta-3/projects/HelloWorld.java
@@ -2,7 +2,7 @@ import lejos.nxt.Button;
 
 public class HelloWorld {
   public static void main (String[] args) {
-    System.out.println("Hola Mundo Positivo 2");
+    System.out.println("Hola Mundo MAC osX");
     Button.waitForAnyPress();
   }
 }
